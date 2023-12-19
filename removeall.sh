7134fe5015b5d26a015aea092660ef3b1ec51f71
Gordon <46924906+FGadvancer@users.noreply.github.com>
2023-12-19 11:43:17 +0800

    fix: online notifications do not push to herself.

--- removeall.sh
+++ removeall.sh
@@ -24,15 +24,15 @@ if [ $? -eq 0 ]; then
     docker-compose -f docker-compose-a.yml up -d && \
     make start
 
-    nohup /data/Open-IM-Server_output/bin/platforms/linux/amd64/openim-push --port 30171 -c /data/Open-IM-Serverconfig/ --prometheus_port 40171 >> openIM.log 2>&1 &
-     nohup /data/Open-IM-Server_output/bin/platforms/linux/amd64/openim-push --port 30172 -c /data/Open-IM-Serverconfig/ --prometheus_port 40172 >> openIM.log 2>&1 &
-       nohup /data/Open-IM-Server_output/bin/platforms/linux/amd64/openim-push --port 30173 -c /data/Open-IM-Serverconfig/ --prometheus_port 40173 >> openIM.log 2>&1 &
-          nohup /data/Open-IM-Server_output/bin/platforms/linux/amd64/openim-push --port 30174 -c /data/Open-IM-Serverconfig/ --prometheus_port 40174 >> openIM.log 2>&1 &
-              nohup /data/Open-IM-Server_output/bin/platforms/linux/amd64/openim-push --port 30175 -c /data/Open-IM-Serverconfig/ --prometheus_port 40175 >> openIM.log 2>&1 &
-               nohup /data/Open-IM-Server_output/bin/platforms/linux/amd64/openim-push --port 30176 -c /data/Open-IM-Serverconfig/ --prometheus_port 40176 >> openIM.log 2>&1 &
-                 nohup /data/Open-IM-Server_output/bin/platforms/linux/amd64/openim-push --port 30177 -c /data/Open-IM-Serverconfig/ --prometheus_port 40177 >> openIM.log 2>&1 &
-#          nohup /data/Open-IM-Server_output/bin/platforms/linux/amd64/openim-rpc-msg --port 30131 -c /data/Open-IM-Serverconfig --prometheus_port 40131 >> openIM.log 2>&1 &
-#        nohup /data/Open-IM-Server_output/bin/platforms/linux/amd64/openim-rpc-msg --port 30132 -c /data/Open-IM-Serverconfig --prometheus_port 40132 >> openIM.log 2>&1 &
-#        nohup /data/Open-IM-Server_output/bin/platforms/linux/amd64/openim-rpc-msg --port 30133 -c /data/Open-IM-Serverconfig --prometheus_port 40133 >> openIM.log 2>&1 &
-#        nohup /data/Open-IM-Server_output/bin/platforms/linux/amd64/openim-rpc-msg --port 30134 -c /data/Open-IM-Serverconfig --prometheus_port 40134 >> openIM.log 2>&1 &
+    nohup /data/Open-IM-Server/_output/bin/platforms/linux/amd64/openim-push --port 30171 -c /data/Open-IM-Server/config/ --prometheus_port 40171 >> openIM.log 2>&1 &
+     nohup /data/Open-IM-Server/_output/bin/platforms/linux/amd64/openim-push --port 30172 -c /data/Open-IM-Server/config/ --prometheus_port 40172 >> openIM.log 2>&1 &
+       nohup /data/Open-IM-Server/_output/bin/platforms/linux/amd64/openim-push --port 30173 -c /data/Open-IM-Server/config/ --prometheus_port 40173 >> openIM.log 2>&1 &
+          nohup /data/Open-IM-Server/_output/bin/platforms/linux/amd64/openim-push --port 30174 -c /data/Open-IM-Server/config/ --prometheus_port 40174 >> openIM.log 2>&1 &
+              nohup /data/Open-IM-Server/_output/bin/platforms/linux/amd64/openim-push --port 30175 -c /data/Open-IM-Server/config/ --prometheus_port 40175 >> openIM.log 2>&1 &
+               nohup /data/Open-IM-Server/_output/bin/platforms/linux/amd64/openim-push --port 30176 -c /data/Open-IM-Server/config/ --prometheus_port 40176 >> openIM.log 2>&1 &
+                 nohup /data/Open-IM-Server/_output/bin/platforms/linux/amd64/openim-push --port 30177 -c /data/Open-IM-Server/config/ --prometheus_port 40177 >> openIM.log 2>&1 &
+#          nohup /data/Open-IM-Server/_output/bin/platforms/linux/amd64/openim-rpc-msg --port 30131 -c /data/Open-IM-Server/config --prometheus_port 40131 >> openIM.log 2>&1 &
+#        nohup /data/Open-IM-Server/_output/bin/platforms/linux/amd64/openim-rpc-msg --port 30132 -c /data/Open-IM-Server/config --prometheus_port 40132 >> openIM.log 2>&1 &
+#        nohup /data/Open-IM-Server/_output/bin/platforms/linux/amd64/openim-rpc-msg --port 30133 -c /data/Open-IM-Server/config --prometheus_port 40133 >> openIM.log 2>&1 &
+#        nohup /data/Open-IM-Server/_output/bin/platforms/linux/amd64/openim-rpc-msg --port 30134 -c /data/Open-IM-Server/config --prometheus_port 40134 >> openIM.log 2>&1 &
 fi
